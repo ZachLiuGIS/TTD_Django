c271eb80ff9f96437ffc9ce20bc69c90cccaf80a
ZachLiuGIS <zachliugis@gmail.com>
2015-05-09 23:31:15 -0400

    rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,7 +7,7 @@ class FunctionalTest(FunctionalTest):
         #Edith goes to the home page and accidentally tries to submit
         #an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
-        inbox = self.browser.find_element_by_id('id_new_item')
+        inbox = self.get_item_input_box() 
         inbox.send_keys('\n')
 
         #The home page refreshes, and there is an error message saying
@@ -16,12 +16,12 @@ class FunctionalTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         #She tries again with some text for the item, which now works
-        inbox = self.browser.find_element_by_id('id_new_item')
+        inbox = self.get_item_input_box() 
         inbox.send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
         #Perversely, she now decides to submit a second blank list item
-        inbox = self.browser.find_element_by_id('id_new_item')
+        inbox = self.get_item_input_box() 
         inbox.send_keys('\n')
 
         #She receives a similar warning on the list page
@@ -30,7 +30,7 @@ class FunctionalTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         #And she can correct it by filling some text in
-        inbox = self.browser.find_element_by_id('id_new_item')
+        inbox = self.get_item_input_box() 
         inbox.send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
